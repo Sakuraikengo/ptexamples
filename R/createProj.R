fa33dd33bf3f27074285c445b21f363ba43b25c0
Sakuraikengo <49226146+Sakuraikengo@users.noreply.github.com>
2023-07-20 16:54:40 +0900

    update template

--- R/createProj.R
+++ R/createProj.R
@@ -6,18 +6,40 @@ createProj <- function(path, ...) {
   params <- list(...)
 
   # create .R file
-  file <- paste0(path, "/analysis.R")
+  file <- paste0(path, "/scripts/1.0.function.R")
   file.create(file)
 
-  # create "data" dir
-  if (params[["data"]]) {
-    dtaDir <- paste0(path, "/data")
+  # create "genotype" dir
+  if (params[["geno"]]) {
+    dtaDir0 <- paste0(path, "/raw_data/genotype")
+    dtaDir1 <- paste0(path, "/data/genotype")
+    dir.create(dtaDir0, recursive = TRUE, showWarnings = FALSE)
+    dir.create(dtaDir1, recursive = TRUE, showWarnings = FALSE)
+  }
+
+    # create "phenotype" dir
+  if (params[["pheno"]]) {
+    dtaDir0 <- paste0(path, "/raw_data/phenotype")
+    dtaDir1 <- paste0(path, "/data/phenotype")
+    dir.create(dtaDir0, recursive = TRUE, showWarnings = FALSE)
+    dir.create(dtaDir1, recursive = TRUE, showWarnings = FALSE)
+  }
+
+  # create "reference" dir
+  if (params[["ref"]]) {
+    dtaDir <- paste0(path, "/reference")
     dir.create(dtaDir, recursive = TRUE, showWarnings = FALSE)
   }
 
-  # create "outputs" dir
-  if (params[["out"]]) {
-    outDir <- paste0(path, "/outputs")
+  # create "midstream" dir
+  if (params[["mid"]]) {
+    dtaDir <- paste0(path, "/midstream")
+    dir.create(dtaDir, recursive = TRUE, showWarnings = FALSE)
+  }
+
+  # create "results" dir
+  if (params[["res"]]) {
+    outDir <- paste0(path, "/results")
     dir.create(outDir, recursive = TRUE, showWarnings = FALSE)
   }
 }
